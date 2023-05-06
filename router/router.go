cc61c122fc8e17bf91a96a8fb82f9ef0b510bec2
lichengyuan123 <1074873702@qq.com>
2023-05-06 17:30:55 +0800

    12213

--- router/router.go
+++ router/router.go
@@ -2,27 +2,18 @@ package router
 
 import (
 	"study/app/api"
-	"time"
 
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
-	"github.com/gogf/gf/os/gsession"
 )
 
 func init() {
 	s := g.Server()
-	s.SetConfigWithMap(g.Map{
-		"SessionMaxAge":  time.Minute,
-		"SessionStorage": gsession.NewStorageRedis(g.Redis()),
-	})
 
 	s.Group("/", func(group *ghttp.RouterGroup) {
 
-		group.ALL("/redisTest", api.RedisTest)
-
 		group.ALL("/hello", api.Hello)
 
-		
 	})
 
 	s.SetPort(8199)
